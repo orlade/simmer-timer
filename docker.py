504a2034ea6429b198e22afa89baa47bb7ddc95a
root <root@ip-172-31-12-253.ap-southeast-2.compute.internal>
2014-10-30 06:13:16 +0000

    Fixed Docker execution.

--- docker.py
+++ docker.py
@@ -1,6 +1,6 @@
 import sumo
 import altruism
-from util import call, calls_to_string
+from util import ROOT, call, calls_to_string
 
 SUMO_IMAGE = 'similitude/sumo-simmer'
 ALTRUISM_IMAGE = 'quay.io/simmer/netlogo-sample'
@@ -10,19 +10,17 @@ IMAGES = [SUMO_IMAGE, ALTRUISM_IMAGE]
 
 
 def run_sumo():
-    opts = ['-v', '.:/opt/profile:ro']
-    docker_run_all(SUMO_IMAGE, sumo.randomDayHourly_calls('/opt/profile'), opts)
+    docker_run_all(SUMO_IMAGE, sumo.randomDayHourly_calls())
 
 
 def run_altruism():
-    out_path = '/out.csv'
-    calls = altruism.altruism_calls(out_path)
+    calls = altruism.altruism_calls()
     return docker_run_all(ALTRUISM_IMAGE, calls)
 
 
 def docker_run_all(image,  calls, opts=[]):
-    opts += ['--entrypoint', '/bin/sh']
-    return docker_run(image, opts, ['-c', '"%s"' % calls_to_string(calls)])
+    opts += ['-v', '%s:%s' % (ROOT,ROOT), '--entrypoint', '/bin/sh']
+    return docker_run(image, ['-c', '%s' % calls_to_string(calls)], opts)
 
 
 def docker_run(image, args, opts=[]):
